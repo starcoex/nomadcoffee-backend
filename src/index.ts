ffaed4ca4d6cd8f1f824bf9990962e6e6ce0e204
Starcoex <starcoex@naver.com>
2023-07-03 21:56:04 +0900

    "Graphql part1"

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+const todos = [
+  {
+    text: "todo",
+  },
+  {
+    text: "todo2",
+  },
+];
+
+const typeDefs = `
+type Todo {
+    text:String
+}
+type Query {
+    todos : [Todo]
+}
+`;
+const resolvers = {
+  Query: {
+    todos: () => todos,
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
+
+console.log(`🚀  Server ready at: ${url}`);
