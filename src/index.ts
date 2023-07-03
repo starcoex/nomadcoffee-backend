5994882fb17930860a7779d15a2f4fdcc77987ea
Starcoex <starcoex@naver.com>
2023-07-04 00:09:48 +0900

    "prisma setup part1"

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,34 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 
-const todos = [
-  {
-    text: "todo",
-  },
-  {
-    text: "todo2 asfas",
-  },
-];
-
 const typeDefs = `
-  type Todo {
-    text: String
+  type Movie {
+    title: String
+    year: Int
   }
   type Query {
-    todos: [Todo]
+    movies: [Movie]
+    movie: Movie
+  }
+  type Mutation {
+    createMovie(title:String): Boolean
+    deleteMovie(title:String): Boolean
   }
 `;
 const resolvers = {
   Query: {
-    todos: () => todos,
+    movies: () => [],
+    movie: () => ({ title: "Movie", year: 40 }),
+  },
+  Mutation: {
+    createMovie: (root, args, context, info) => {
+      console.log(root, args, context, info);
+      return true;
+    },
+    deleteMovie: (root, args, context, info) => {
+      console.log(root, args, context, info);
+      return true;
+    },
   },
 };
 
