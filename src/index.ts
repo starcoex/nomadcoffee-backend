a20752fc64590a69d4307314b80f4f68c27ea5c6
starcoex <69073390+starcoex@users.noreply.github.com>
2023-07-04 11:24:57 +0900

    "prisma client setup part2"

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,55 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
+import { Movie, PrismaClient } from "@prisma/client";
+
+const client = new PrismaClient();
 
 const typeDefs = `
   type Movie {
-    title: String
-    year: Int
+    id: Int!
+    title: String!
+    year: Int!
+    genre: String
+    createdAt: String!
+    updatedAt: String!
   }
   type Query {
     movies: [Movie]
-    movie: Movie
+    movie(id:Int!): Movie
   }
   type Mutation {
-    createMovie(title:String): Boolean
-    deleteMovie(title:String): Boolean
+    createMovie(title: String!, year:Int!, genre:String): Movie
+    deleteMovie(title: Int!): Movie
+    updateMovie(id:Int!, year:Int!): Movie
   }
 `;
 const resolvers = {
   Query: {
-    movies: () => [],
-    movie: () => ({ title: "Movie", year: 40 }),
+    movies: () => client.movie.findMany(),
+    movie: (_, { id }: Movie) => {
+      client.movie.findFirst({ where: { id } });
+    },
   },
   Mutation: {
-    createMovie: (root, args, context, info) => {
-      console.log(root, args, context, info);
-      return true;
+    createMovie: (_, { title, year, genre }: Movie) => {
+      return client.movie.create({
+        data: {
+          title,
+          year,
+          genre,
+        },
+      });
     },
-    deleteMovie: (root, args, context, info) => {
-      console.log(root, args, context, info);
-      return true;
+    deleteMovie: (_, { id }: Movie) => {
+      return client.movie.delete({ where: { id } });
+    },
+    updateMovie: (_, { id, year }: Movie) => {
+      return client.movie.update({
+        where: { id },
+        data: {
+          year,
+        },
+      });
     },
   },
 };
